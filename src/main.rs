51fb7efaab3542ae244b72d5e67ffecd0176924c
tatjam <tatjam02@gmail.com>
2025-07-05 21:46:24 +0200

    Add power supply voltage check
    For safety during high speed GPIO operation, as otherwise device could
    be damaged.

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,10 @@ fn setup_pll_autochirp(rcc: &mut stm32h7s::RCC) {
 
     // TODO: Later on we may want to use HSE to reduce jitter
     // Output PLL on MCO1, which can be connected to pll1_q_ck
+    // (MCO1 is the alternate function 0 for PA8)
+    // TODO: Maybe set prescaler to 1 instead of disabled?
+    rcc.pllcfgr().write(|w| w.divq1en().enabled());
+    rcc.cfgr().write(|w| w.mco1().pll1_q());
 
     // Set predividers to get good reference clock (ex. divide HSI, 64MHz, by 32 to get 2MHz)
     // NOTE: HSI is selected by default on reset as PLLSRC
@@ -72,13 +76,45 @@ fn setup_pll_autochirp(rcc: &mut stm32h7s::RCC) {
 
 }
 
+// Returns true if power supply is below 2V, which gurantees fast GPIO is safe
+fn powersupply_okay(pwr: &mut stm32h7s::PWR) -> bool {
+
+    // Setup threshold for PVD (Programmable Voltage Detector)
+    // (Level 0 is around 2V)
+    pwr.cr1().write(|w| unsafe{ w.pls().bits(0) });
+
+    // Launch the PVD
+    pwr.cr1().write(|w| w.pvde().set_bit());
+
+    // Wait a bit, just in case supply is stabilizing
+    for i in 0..100000 {
+        core::hint::black_box(i);
+    }
+
+    // Check if the supply voltage is okay (less than maximum)
+    // (PVD gets true if voltage is below trigger!)
+    let okay = pwr.sr1().read().pvdo().bit_is_set();
+
+    pwr.cr1().write(|w| w.pvde().clear_bit());
+
+    return okay;
+}
+
 // This marks the entrypoint of our application. The cortex_m_rt creates some
 // startup code before this, but we don't need to worry about this. otably, it
 // enables the FPU
 #[entry]
 fn main() -> ! {
     let mut periph = stm32h7s::Peripherals::take().unwrap();
-    defmt::info!("Hello!");
+    defmt::info!("Hello directrf!");
+
+    if !powersupply_okay(&mut periph.PWR) {
+        defmt::error!("Power supply voltage too high. Make sure you use 1.8V.");
+       loop {}
+    }
+    else {
+        defmt::info!("Power supply voltage check okay");
+    }
 
     setup_pll_autochirp(&mut periph.RCC);
 
