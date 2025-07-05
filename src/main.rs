b8c0d87f34d7b945fa227ec65c394e83d14f8c6a
tatjam <tatjam02@gmail.com>
2025-07-05 17:47:28 +0200

    Fighting the linker, instant HardFault after program start

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,27 @@
 #![no_std]
 #![no_main]
 
-use core::ops::BitOr;
-use panic_halt as _;
-use stm32h7::{stm32h7s, Periph};
+
+use defmt_rtt as _;
+use panic_probe as _;
+use stm32h7::{stm32h7s};
+
 use cortex_m_rt::entry;
 
 // Uses spread spectrum to "automatically" generate chirps
 fn setup_pll_autochirp(rcc: &mut stm32h7s::RCC) {
+    // Configure these as desired
     let desired_center_freq: f64 = 435_000_000.0;
     let input_freq: f64 = 64_000_000.0;
-    let div1m_float: f64 = input_freq / 2_000_000.0;
-    // Make sure the input frequency is not too high, as otherwise we couldn't divide it down
-    assert!(div1m_float < 64.0);
-    let div1m = libm::floor(div1m_float) as u8;
-    assert!(libm::fabs(libm::floor(div1m_float) - div1m_float) < 0.001);
+    let divided_freq: f64 = 2_000_000.0;
+    assert!(divided_freq >= 1_000_000.0 && divided_freq <= 16_000_000.0);
 
+    let div1m = libm::floor(input_freq / divided_freq) as u8;
+    assert!(div1m <= 63, "PLL divm too high, lower input freq or increase divided_freq.");
+
+    let plldivn = libm::floor(desired_center_freq / 2_000_000.0) as u16;
+    assert!(plldivn >= 8 && plldivn <= 420,
+            "PLL divn too high, lower center freq or increase divided_freq.");
 
 
     // TODO: Later on we may want to use HSE to reduce jitter
@@ -24,7 +30,17 @@ fn setup_pll_autochirp(rcc: &mut stm32h7s::RCC) {
     // Set predividers to get good reference clock (ex. divide HSI, 64MHz, by 32 to get 2MHz)
     // NOTE: HSI is selected by default on reset as PLLSRC
     // NOTE: This allows us to leave default PLL1RGE value, which assumes input freq is from 1 to 2MHz
-    rcc.pllckselr().write(|w| w.divm1().set(32));
+    rcc.pllckselr().write(|w| w.divm1().set(div1m));
+
+    if divided_freq <= 2_000_000.0 {
+        rcc.pllcfgr().write(|w| w.pll1rge().range1());
+    } else if divided_freq <= 4_000_000.0 {
+        rcc.pllcfgr().write(|w| w.pll2rge().range2());
+    } else if divided_freq <= 8_000_000.0 {
+        rcc.pllcfgr().write(|w| w.pll2rge().range4());
+    } else if divided_freq <= 16_000_000.0 {
+        rcc.pllcfgr().write(|w| w.pll2rge().range8());
+    }
 
     // Make sure the sigma-delta modulator (SDM) is loaded with 0.
     // Procedure taken from manual, may not be needed as this seems to be the default value?
@@ -40,35 +56,34 @@ fn setup_pll_autochirp(rcc: &mut stm32h7s::RCC) {
     // TODO: Is this next call required? Doesn't appear to be
     rcc.pllcfgr().write(|w| w.pll1fracen().clear_bit());
 
-    // Setup nominal PLL frequency on the 435MHz band, so we have a "safe" bandwidth of 10MHz
-    // To do so, we multiply the input by 435, i.e. divide the input by that
+    rcc.pll1divr1().write(|w| unsafe{ w.divn1().bits(plldivn) });
 
 
     /*
     // Setup clock spreading for chirps
     rcc.pllcfgr().write(|w| w.pll1sscgen().set_bit());
-
     */
 
     // Enable PLL
     rcc.cr().write(|w| w.pll1on().set_bit());
 
     // Wait for PLL ready
-    while(rcc.cr().read().pll1rdy().bit_is_clear()) {}
+    while rcc.cr().read().pll1rdy().bit_is_clear() {}
 
 }
 
 // This marks the entrypoint of our application. The cortex_m_rt creates some
-// startup code before this, but we don't need to worry about this, notably, it
+// startup code before this, but we don't need to worry about this. otably, it
 // enables the FPU
 #[entry]
 fn main() -> ! {
-    let mut periph = stm32h7s::Peripherals::take().unwrap();
+    //let mut periph = stm32h7s::Peripherals::take().unwrap();
+    //defmt::println!("Guamdo!");
 
-    // Enable the PLL clock and output it on MCO
-    setup_pll_autochirp(&mut periph.RCC);
+    //setup_pll_autochirp(&mut periph.RCC);
 
     loop {
-        
+
+      //  defmt::error!("Hello, world!");
     }
 }
