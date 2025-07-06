477811bbd0e951a764f0a923b1f31a37538edac7
tatjam <tatjam02@gmail.com>
2025-07-06 12:21:11 +0200

    RF output working well up to the GHz
    It turns out a small footgun in Rust embedded was causing all issues

--- src/autochirp.rs
+++ src/autochirp.rs
@@ -3,46 +3,36 @@ use stm32h7::{stm32h7s};
 // Uses spread spectrum to "automatically" generate chirps
 pub fn setup_pll(rcc: &mut stm32h7s::RCC) {
     // Configure these as desired
-    let desired_center_freq: f64 = 435_000_000.0;
-    let input_freq: f64 = 64_000_000.0;
-    let divided_freq: f64 = 2_000_000.0;
+    let desired_center_freq: f64 = 1000_000_000.0;
+    // (HSE on NUCLEO board)
+    let input_freq: f64 = 24_000_000.0;
+    let divided_freq: f64 = 4_000_000.0;
     assert!(divided_freq >= 1_000_000.0 && divided_freq <= 16_000_000.0);
 
+    // DIV1M has to divide input_freq to get divided_freq, so...
     let div1m = libm::floor(input_freq / divided_freq) as u8;
     assert!(div1m <= 63, "PLL divm too high, lower input freq or increase divided_freq.");
 
-    let plldivn = libm::floor(desired_center_freq / 2_000_000.0) as u16;
+    // DIVN has to MULTIPLY divided_freq to get desired_freq / 2 (an aditional 2 divider is
+    // at the output of the fast VCO)
+    let plldivn = libm::floor(desired_center_freq / divided_freq - 1.0) as u16;
     assert!(plldivn >= 8 && plldivn <= 420,
             "PLL divn too high, lower center freq or increase divided_freq.");
 
+    defmt::info!("Using div1m = {}, plldivn = {}, actual center freq {}", div1m, plldivn,
+        input_freq / (div1m as f64) * (plldivn as f64 + 1.0));
 
-    // TODO: Later on we may want to use HSE to reduce jitter
-    // Output PLL on MCO1, which can be connected to pll1_q_ck
-    // (MCO1 is the alternate function 0 for PA8)
-    // TODO: Maybe set prescaler to 1 instead of disabled?
-    rcc.pllcfgr().write(|w| w.divq1en().enabled());
-    rcc.cfgr().write(|w| w.mco1().pll1_q());
-
-    // Set predividers to get good reference clock (ex. divide HSI, 64MHz, by 32 to get 2MHz)
+    rcc.pllckselr().modify(|_, w| w.pllsrc().hse());
+    // Set predividers to get good reference clock
     // NOTE: HSI is selected by default on reset as PLLSRC
     // NOTE: This allows us to leave default PLL1RGE value, which assumes input freq is from 1 to 2MHz
-    rcc.pllckselr().write(|w| w.divm1().set(div1m));
-
-    if divided_freq <= 2_000_000.0 {
-        rcc.pllcfgr().write(|w| w.pll1rge().range1());
-    } else if divided_freq <= 4_000_000.0 {
-        rcc.pllcfgr().write(|w| w.pll2rge().range2());
-    } else if divided_freq <= 8_000_000.0 {
-        rcc.pllcfgr().write(|w| w.pll2rge().range4());
-    } else if divided_freq <= 16_000_000.0 {
-        rcc.pllcfgr().write(|w| w.pll2rge().range8());
-    }
+    rcc.pllckselr().modify(|_, w| w.divm1().set(div1m));
 
     // Make sure the sigma-delta modulator (SDM) is loaded with 0.
     // Procedure taken from manual, may not be needed as this seems to be the default value?
-    rcc.pllcfgr().write(|w| w.pll1fracen().clear_bit());
-    rcc.pll1fracr().write(|w| unsafe{ w.fracn().bits(0)});
-    rcc.pllcfgr().write(|w| w.pll1fracen().set_bit());
+    rcc.pllcfgr().modify(|_, w| w.pll1fracen().clear_bit());
+    rcc.pll1fracr().modify(|_, w| unsafe{ w.fracn().bits(0)});
+    rcc.pllcfgr().modify(|_, w| w.pll1fracen().set_bit());
 
     // Wait for a bit (atleast 5μs)
     for i in 0..100000 {
@@ -50,20 +40,47 @@ pub fn setup_pll(rcc: &mut stm32h7s::RCC) {
     }
 
     // TODO: Is this next call required? Doesn't appear to be
-    rcc.pllcfgr().write(|w| w.pll1fracen().clear_bit());
+    //rcc.pllcfgr().modify(|_, w| w.pll1fracen().clear_bit());
 
-    rcc.pll1divr1().write(|w| unsafe{ w.divn1().bits(plldivn) });
+    rcc.pllcfgr().modify(|_, w| w.pll1sscgen().clear_bit());
+
+    rcc.pllcfgr().modify(|_, w| w.pll1vcosel().clear_bit());
+    // TODO: Later on we may want to use HSE to reduce jitter
+    // Output PLL on MCO1, which can be connected to pll1_q_ck
+    // (MCO1 is the alternate function 0 for PA8)
+    // TODO: Maybe set prescaler to 1 instead of disabled?
+    rcc.cfgr().modify(|_, w| w.mco1().pll1_q().mco1pre().set(1));
+
+
+
+    if divided_freq <= 2_000_000.0 {
+        rcc.pllcfgr().modify(|_, w| w.pll1rge().range1());
+    } else if divided_freq <= 4_000_000.0 {
+        rcc.pllcfgr().modify(|_, w| w.pll2rge().range2());
+    } else if divided_freq <= 8_000_000.0 {
+        rcc.pllcfgr().modify(|_, w| w.pll2rge().range4());
+    } else if divided_freq <= 16_000_000.0 {
+        rcc.pllcfgr().modify(|_, w| w.pll2rge().range8());
+    }
+
+
+    rcc.pll1divr1().modify(|_, w| unsafe{ w.divn1().bits(plldivn) });
 
 
     /*
     // Setup clock spreading for chirps
-    rcc.pllcfgr().write(|w| w.pll1sscgen().set_bit());
+    rcc.pllcfgr().modify(|_, w| w.pll1sscgen().set_bit());
     */
 
     // Enable PLL
-    rcc.cr().write(|w| w.pll1on().set_bit());
+    rcc.cr().modify(|_, w| w.pll1on().set_bit());
 
     // Wait for PLL ready
     while rcc.cr().read().pll1rdy().bit_is_clear() {}
+
+    // Enable outputs
+    rcc.pllcfgr().modify(|_, w| w.divq1en().enabled());
+
+    defmt::info!("PLL is ready!");
 
 }
--- src/main.rs
+++ src/main.rs
@@ -16,10 +16,10 @@ fn powersupply_okay(pwr: &mut stm32h7s::PWR) -> Option<PowerSupplyGuarantee> {
 
     // Setup threshold for PVD (Programmable Voltage Detector)
     // (Level 0 is around 2V)
-    pwr.cr1().write(|w| unsafe{ w.pls().bits(0) });
+    pwr.cr1().modify(|_, w| unsafe{ w.pls().bits(0) });
 
     // Launch the PVD
-    pwr.cr1().write(|w| w.pvde().set_bit());
+    pwr.cr1().modify(|_, w| w.pvde().set_bit());
 
     // Wait a bit, just in case supply is stabilizing
     for i in 0..100000 {
@@ -30,13 +30,24 @@ fn powersupply_okay(pwr: &mut stm32h7s::PWR) -> Option<PowerSupplyGuarantee> {
     // (PVD gets true if voltage is below trigger!)
     let okay = pwr.sr1().read().pvdo().bit_is_set();
 
-    pwr.cr1().write(|w| w.pvde().clear_bit());
+    pwr.cr1().modify(|_, w| w.pvde().clear_bit());
 
     if okay {
         return Some(PowerSupplyGuarantee {});
     }
 
     return None;
+}
+
+// Assumes we are on a NUCLEO board, which has a 24MHz clock source connected to HSE
+fn setup_hse(rcc: &mut stm32h7s::RCC) {
+    defmt::info!("Starting HSE");
+    rcc.cr().modify(|_, w| w.hseon().set_bit());
+
+    // Wait for HSE ready
+    while !rcc.cr().read().hserdy().bit_is_set() {}
+
+    defmt::info!("HSE is ready");
 }
 
 // Launches GPIO peripheral and setups GPIO PA8 for fastest possible operation,
@@ -46,12 +57,12 @@ fn setup_gpio(periph: &mut stm32h7s::Peripherals, guarantee: PowerSupplyGuarante
     let gpioa = &mut periph.GPIOA;
 
     // Enable the gpioa peripheral
-    rcc.ahb4enr().write(|w| w.gpioaen().enabled());
+    rcc.ahb4enr().modify(|_, w| w.gpioaen().enabled());
 
     // Configure PA8 for special function
-    gpioa.moder().write(|w| w.mode8().alternate());
+    gpioa.moder().modify(|_, w| w.mode8().alternate());
     // Set it for highest speed operation
-    gpioa.ospeedr().write(|w| w.ospeed8().very_high_speed());
+    //gpioa.ospeedr().modify(|_, w| w.ospeed8().very_high_speed());
 
 }
 
@@ -69,6 +80,7 @@ fn main() -> ! {
         loop {}
     }
 
+    setup_hse(&mut periph.RCC);
     setup_gpio(&mut periph, powersupply_guarantee.unwrap());
     autochirp::setup_pll(&mut periph.RCC);
 
